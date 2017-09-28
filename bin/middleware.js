56146a6d85bd0992d8ae1740360ec30bc05aed9b
James <james.speirs@gmail.com>
2017-09-28 10:59:26 -0600

    use debug for logging

--- bin/middleware.js
+++ bin/middleware.js
@@ -15,34 +15,24 @@
  *    limitations under the License.
  **/
 'use strict';
-const Log       = require('./log');
-const proxy     = require('./proxy');
+const debug     = require('debug')('sans-server-middleware');
 
 module.exports = Middleware;
 
 /**
  * Create a new middleware instance.
  * @params {string} [name='']
- * @params {object} [config]
  * @returns {Middleware}
  * @constructor
  */
-function Middleware(name, config) {
+function Middleware(name) {
     if (!(this instanceof Middleware)) return new Middleware(name);
     this.name = name || '';
-
-    config = !config || typeof config !== 'object' ? {} : JSON.parse(JSON.stringify(config));
-    if (!config.log || typeof config.log !== 'object') config.log = {};
-    if (!config.log.hasOwnProperty('advanced')) config.log.advanced = true;
-    if (!config.log.hasOwnProperty('index')) config.log.index = false;
-    if (!config.log.hasOwnProperty('details')) config.log.details = false;
-    if (!proxy.supported) config.log.advanced = false;
 
     Object.defineProperty(this, '_', {
         enumerable: false,
         configurable: false,
         value: {
-            config: config,
             length: 0,
             ordered: null,
             weights: new Map()
@@ -65,7 +55,6 @@ function Middleware(name, config) {
  */
 Middleware.prototype.add = function(weight, hook) {
     const _ = this._;
-    const advanced = this._.config.log.advanced;
 
     // handle variable parameters
     if (typeof arguments[0] === 'function') {
@@ -86,30 +75,27 @@ Middleware.prototype.add = function(weight, hook) {
     const errHandler = hook.length >= 4;
 
     // create a wrapper around the middleware
-    const wrapped = function(parentLog, err, req, res, next) {
-        const log = parentLog.nest('middleware ' + name);
-        const r = proxy.request(req, advanced, log);
+    const wrapped = function(err, req, res, next) {
 
-        function done(err) {
-            if (!advanced) log.event('middleware end', name);
-            log.release();
-            next(err);
+        function done(error) {
+            debug('%s middleware-end %s', req.id, name);
+            next(error);
         }
 
         if (err && !errHandler) {
-            log.event('skipped', 'Hook ' + (advanced ? '' : name + ' ') + 'is not for error handling');
+            debug('%s skipped %s Hook is not for error handling', req.id, name);
             next(err);
 
         } else if (!err && errHandler) {
-            log.event('skipped', 'Hook ' + (advanced ? '' : name + ' ') + 'is for error handling');
+            debug('%s skipped %s Hook is for error handling', req.id, name);
             next();
 
         } else {
             try {
-                if (!advanced) log.event('middleware start', name);
-                errHandler ? hook(err, r, res, done) : hook(r, res, done);
+                debug('%s middleware-start %s', req.id, name);
+                errHandler ? hook(err, req, res, done) : hook(res, res, done);
             } catch (err) {
-                next(err);
+                done(err);
             }
 
         }
@@ -184,32 +170,22 @@ Middleware.prototype.sort = function() {
 
 function run(middleware, req, res, reverse) {
     if (!middleware._.ordered) middleware.sort();
-    const config = middleware._.config;
     const chain = middleware._.ordered.concat();
-    const isRoot = !req.log.nest;
-    const log = isRoot
-        ? new Log('hook-runner', config.log)
-        : req.log.nest('hook-runner');
-    const prefix = config.log.advanced ? '' : 'hook-';
 
     return new Promise((resolve, reject) => {
         function next(err) {
             const callback = chain[reverse ? 'pop' : 'shift']();
             if (callback) {
-                callback(log, err, req, res, next);
+                callback(err, req, res, next);
             } else if (err) {
-                log.event(prefix + 'rejected', middleware.name);
-                log.release();
-                if (isRoot) req.emit('log-report', log.report());
+                debug('%s hook-runner rejected %s', req.id, middleware.name);
                 reject(err);
             } else {
-                log.event(prefix + 'resolved', middleware.name);
-                log.release();
-                if (isRoot) req.emit('log-report', log.report());
+                debug('%s hook-runner resolved %s', req.id, middleware.name);
                 resolve();
             }
         }
-        log.event(reverse ? prefix + 'reverse-run' : prefix + 'run', middleware.name);
+        debug('%s hook-runner %s %s', req.id, reverse ? 'reverse-run' : 'run', middleware.name);
         next();
     });
 }
